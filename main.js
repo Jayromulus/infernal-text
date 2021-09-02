66e078dccc880cacbb794c4f2a44a5be6ccdd096
Jayromulus <jerbear2@gmail.com>
2021-09-02 15:21:14 -0400

    new styles

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ input.addEventListener('input', (e) => {
   const original = e.target.value;
 
   const library = [
-    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '],
-    ['m','f','s','d','l','z','v','r','n','t','c','b','a','j','e','w','t','g','i','h','p','w', '','q','o','x',' ']
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '],
+    ['m', 'f', 's', 'd', 'l', 'z', 'v', 'r', 'n', 't', 'c', 'b', 'a', 'j', 'e', 'w', 't', 'g', 'i', 'h', 'p', 'w', '', 'q', 'o', 'x', ' ']
   ]
   // console.log(library[0].length)
   let translated = '';
-  for(letter of e.target.value){
-    // console.log(library[0].indexOf(letter))
-    translated += library[1][library[0].indexOf(letter)]
+  for (letter of e.target.value) {
+    if (letter.match(/^[\w\s]+$/)) {
+      // console.log(library[0].indexOf(letter))
+      translated += library[1][library[0].indexOf(letter.toLowerCase())]
+    } else {
+      translated += ''
+    }
   }
   output.innerText = translated
 })
